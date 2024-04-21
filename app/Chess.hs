fd204ab6288ddf5aea6b021edabc14c22fb664d8
Logan M <>
2024-04-21 16:29:51 +1200

    Board show instance, default board setup variable

--- app/Chess.hs
+++ app/Chess.hs
@@ -5,6 +5,15 @@ module Chess (
 import qualified Data.Map as M
 
 data Piece = WhitePawn | WhiteKnight | WhiteBishop | WhiteRook | WhiteQueen | WhiteKing |
-             BlackPawn | BlackKnight | BlackBishop | BlackRook | BlackQueen | BlackKing
+             BlackPawn | BlackKnight | BlackBishop | BlackRook | BlackQueen | BlackKing deriving (Show, Eq)
 
-type Board = M.Map (Int, Int) Piece 
+newtype Board = Board (M.Map (Int, Int) Piece)
+
+instance Show Board where
+    show (Board m) = show . map (\((a,b),p) -> (['a'..'h']!!(a-1):show b, p)) . M.toList $ m 
+
+newBoard :: Board
+newBoard = Board . M.fromList $ zip (zip [1..8] (replicate 8 1)) [WhiteRook, WhiteKnight, WhiteBishop, WhiteQueen, WhiteKing, WhiteBishop, WhiteKnight, WhiteRook] ++
+                                zip (zip [1..8] (replicate 8 2)) (replicate 8 WhitePawn) ++
+                                zip (zip [1..8] (replicate 8 7)) [BlackRook, BlackKnight, BlackBishop, BlackQueen, BlackKing, BlackBishop, BlackKnight, BlackRook] ++
+                                zip (zip [1..8] (replicate 8 8)) (replicate 8 BlackPawn)
