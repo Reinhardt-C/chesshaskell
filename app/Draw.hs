a53d257aa1e14d8f3056736d7ec7044b6ed72139
Logan M <>
2024-04-22 11:22:34 +1200

    little comment

--- app/Draw.hs
+++ app/Draw.hs
@@ -4,6 +4,7 @@ module Draw (
 
 import Graphics.Gloss
 
+-- 320x320 board
 board :: Picture
 board = translate (-140) (-140) . pictures $ map (\n -> translate ((*40) . fromIntegral $ n`div`8) ((*40) . fromIntegral $ mod n 8) $ if even (n-n`div`8) then lightSquare else darkSquare) [0..63]
 
